5a67c0387b95399867359588572ebcdcb2229ba6
Luizfer-Col <luizfercolorado@gmail.com>
2025-06-11 12:19:21 -0500

    feat(db): set up SQLite database with Drizzle ORM for expo
    - Add initial database schema and migrations
    - Configure babel and metro for SQL file support
    - Implement database initialization and migration handling
    - Update app layout to handle database initialization

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,26 @@
+import migrations from '@/drizzle/migrations';
+import db, { DATABASE_NAME } from '@/src/core/db';
+import { useMigrations } from 'drizzle-orm/expo-sqlite/migrator';
 import { Stack } from 'expo-router';
+import * as SQLite from 'expo-sqlite';
+import { ActivityIndicator, Text, View } from 'react-native';
 
 export default function RootLayout() {
-  return <Stack />;
+  const { success, error } = useMigrations(db, migrations);
+
+  if (error) {
+    return <Text>{error.message}</Text>;
+  }
+  if (!success) {
+    return (
+      <View>
+        <ActivityIndicator />
+      </View>
+    );
+  }
+  return (
+    <SQLite.SQLiteProvider databaseName={DATABASE_NAME}>
+      <Stack />
+    </SQLite.SQLiteProvider>
+  );
 }
---
+++ src/core/db/index.ts
@@ -0,0 +1,13 @@
+import { drizzle } from 'drizzle-orm/expo-sqlite';
+import * as SQLite from 'expo-sqlite';
+
+export const DATABASE_NAME = 'assignments-manager.db';
+console.log('Opening database:', DATABASE_NAME);
+
+export const expoDB = SQLite.openDatabaseSync(DATABASE_NAME);
+console.log('Database opened successfully');
+
+const db = drizzle(expoDB);
+console.log('Drizzle ORM initialized');
+
+export default db;
