5c72cabfd85f7a44caf67d1ebb3a2cd81862c6e0
Luizfer-Col <luizfercolorado@gmail.com>
2025-06-14 09:35:27 -0500

    feat(metro): add wasm support and COEP/COOP headers
    Add WebAssembly asset support and Cross-Origin headers to enable SharedArrayBuffer functionality

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,21 @@
 const { getDefaultConfig } = require('expo/metro-config');
+const { withNativeWind } = require('nativewind/metro');
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
 config.resolver.sourceExts.push('sql');
+
+// Add wasm asset support
+config.resolver.assetExts.push('wasm');
+
+// Add COEP and COOP headers to support SharedArrayBuffer
+config.server.enhanceMiddleware = middleware => {
+  return (req, res, next) => {
+    res.setHeader('Cross-Origin-Embedder-Policy', 'credentialless');
+    res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+    middleware(req, res, next);
+  };
+};
+
 module.exports = config;
